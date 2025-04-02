8e31b71579b98ec9e6eeede48ff6f3c121ea4b83
abibi189 <145033589+abibi189@users.noreply.github.com>
2025-04-02 18:23:55 +0000

    remove message

--- src/form.jsx
+++ src/form.jsx
@@ -40,15 +40,7 @@ export function SubscriberForm() {
           </Icon>
         </Form.Control>
       </Form.Field>
-      <Form.Field>
-        <Form.Label>Message</Form.Label>
-        <Form.Textarea
-          value={message}
-          onChange={(e) => {
-            return setMessage(e.target.value);
-          }}
-        />
-      </Form.Field>
+
       <Form.Field>
         <Form.Label>Subject</Form.Label>
         <Form.Field kind="group">
